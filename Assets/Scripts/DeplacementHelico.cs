25aa6418fc3e35a712a1f1b12c94f47c1b0e90a1
Matilda <153890630+Bobateaaa@users.noreply.github.com>
2024-09-13 10:00:36 -0400

    collision semi fini

--- Assets/Scripts/DeplacementHelico.cs
+++ Assets/Scripts/DeplacementHelico.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DeplacementHelico : MonoBehaviour
@@ -7,12 +8,17 @@ public class DeplacementHelico : MonoBehaviour
     // Déclarer les variables
     private Rigidbody rb; 
     public GameObject heliceAvant; 
+    public GameObject objetExplosion;
+
     private float vitesseHelice; 
     private bool demarreMoteur;
+
     float vitesseAvant = 0f; 
     float vitesseAvantMax = 10000f; 
     float vitesseTourne = 1000f; 
     float vitesseMonte = 1000f; 
+
+    public bool finJeu;
 
     // Start is called before the first frame update
     void Start()
@@ -27,7 +33,8 @@ public class DeplacementHelico : MonoBehaviour
         rb.useGravity = false;
     }
 
-    // Update is called once per frame
+
+   
     void FixedUpdate()
     {
         // Accéder à la vitesse de rotation de l'hélice avant
@@ -36,44 +43,69 @@ public class DeplacementHelico : MonoBehaviour
         // Accéder à l'état de l'hélice avant
         demarreMoteur = heliceAvant.GetComponent<TourneObjet>().demarreMoteur;
 
-        // Si la vitesse de l'hélice est suffisante, désactiver la gravité et permettre le contrôle
-        if (vitesseHelice > 20f)
+        if(finJeu == false)
         {
-            rb.useGravity = false;
-
-            // Appliquer une force pour monter/descendre
-            var forceMonter = Input.GetAxis("Vertical") * vitesseMonte;
-            rb.AddRelativeForce(Vector3.up * forceMonter);
-
-            // Appliquer un couple pour tourner à gauche/droite
-            var forceTourner = Input.GetAxis("Horizontal") * vitesseTourne;
-            rb.AddRelativeTorque(0, forceTourner, 0);
-
-            // Si la touche E est enfoncée, augmenter la vitesse vers l'avant
-            if (Input.GetKey(KeyCode.E))
+            
+          // Si la vitesse de l'hélice est suffisante, désactiver la gravité et permettre le contrôle
+            if (vitesseHelice > 20f)
             {
-                vitesseAvant += 10f;
-                if (vitesseAvant > vitesseAvantMax)
+                rb.useGravity = false;
+
+                // Appliquer une force pour monter/descendre
+                var forceMonter = Input.GetAxis("Vertical") * vitesseMonte;
+                rb.AddRelativeForce(Vector3.up * forceMonter);
+
+                // Appliquer un couple pour tourner à gauche/droite
+                var forceTourner = Input.GetAxis("Horizontal") * vitesseTourne;
+                rb.AddRelativeTorque(0, forceTourner, 0);
+
+                // Si la touche E est enfoncée, augmenter la vitesse vers l'avant
+                if (Input.GetKey(KeyCode.E))
                 {
-                    vitesseAvant = vitesseAvantMax;
+                    vitesseAvant += 10f;
+                    if (vitesseAvant > vitesseAvantMax)
+                    {
+                        vitesseAvant = vitesseAvantMax;
+                    }
+                    rb.AddRelativeForce(Vector3.forward * vitesseAvant);
                 }
-                rb.AddRelativeForce(Vector3.forward * vitesseAvant);
+                // Si la touche Q est enfoncée, diminuer la vitesse vers l'avant
+                else if (Input.GetKey(KeyCode.Q))
+                {
+                    vitesseAvant -= 10f;
+                    if (vitesseAvant < 0)
+                    {
+                        vitesseAvant = 0;
+                    }
+                    rb.AddRelativeForce(Vector3.forward * vitesseAvant);
+                }
             }
-            // Si la touche Q est enfoncée, diminuer la vitesse vers l'avant
-            else if (Input.GetKey(KeyCode.Q))
+            // Si le moteur n'est pas démarré, activer la gravité
+            else if (!demarreMoteur)
             {
-                vitesseAvant -= 10f;
-                if (vitesseAvant < 0)
-                {
-                    vitesseAvant = 0;
-                }
-                rb.AddRelativeForce(Vector3.forward * vitesseAvant);
+                rb.useGravity = true;
             }
         }
-        // Si le moteur n'est pas démarré, activer la gravité
-        else if (!demarreMoteur)
+        else if(finJeu == true)
         {
-            rb.useGravity = true;
+
         }
     }
+
+    void OnCollisionEnter(Collision collisionTrue)   // le type de la variable est Collision 
+    {
+        if (collisionTrue.gameObject.tag == "terrain") 
+        {
+            finJeu = true;
+            objetExplosion.SetActive(true);
+            rb.useGravity = true;
+            rb.drag = 0;
+            rb.angularDrag = 0;
+            rb.freezeRotation = false;
+
+            
+        } 
+
+    }
+
 }
